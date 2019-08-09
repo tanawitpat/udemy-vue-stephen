60cb81c9a26caad4e85e4aad17761a22ede2db8e
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-08-09 14:55:39 +0700

    Section 1, Lecture 17

--- 02_Template-Instance/app.js
+++ 02_Template-Instance/app.js
@@ -1,8 +1,16 @@
 new Vue({
     el: "#app",
-    methods: {
-        onNewText: function(event) {
-            console.log(event.target.value)
+    data: { // Initialize our list of "ingredients"
+        textInput: ""
+    },
+    computed: { // Turn data into viewable values
+        identicon: function() {
+            return jdenticon.toSvg(this.textInput, 200)
+        }
+    },
+    methods: { // Use these function to change data
+        onInput: function(event) {
+            this.textInput = event.target.value
         }
     }
 })
