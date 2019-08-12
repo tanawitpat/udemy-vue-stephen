cc23c0df9fecf0e162138c5bf21474655e269218
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-08-12 22:08:28 +0700

    Section 4, Lecture 80

--- 07_Image-Storage/src/App.vue
+++ 07_Image-Storage/src/App.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
-        Hello World
+        <AppHeader/>
     </div>
 </template>
 
 <script>
+import AppHeader from "./components/AppHeader"
+
 export default {
-    name: "App"
+    name: "App",
+    components: {
+        AppHeader
+    }
 }
 </script>
