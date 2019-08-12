470a15e1aac24b6fb45792d791a517c5cfda6179
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-08-12 16:09:45 +0700

    Section 3, Lecture 60

--- 04_Youtube/src/components/VideoListItem.vue
+++ 04_Youtube/src/components/VideoListItem.vue
@@ -1,6 +1,6 @@
 <template>
     <li class="list-group-item">
-        <img :src="video.snippet.thumbnails.default.url"/>
+        <img :src="thumbnailUrl"/>
         {{ video.snippet.title }}
     </li>
 </template>
@@ -10,6 +10,11 @@ export default {
     name: "VideoListItem",
     props: {
         video: Object
+    },
+    computed: {
+        thumbnailUrl() {
+            return this.video.snippet.thumbnails.default.url
+        }
     }
 }
 </script>
