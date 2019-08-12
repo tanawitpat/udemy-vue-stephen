78d608264064caf1bc6a4a7732a77858b71852ee
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-08-12 21:02:49 +0700

    Section 3, Lecture 68

--- 04_Youtube/src/components/VideoDetail.vue
+++ 04_Youtube/src/components/VideoDetail.vue
@@ -1,6 +1,9 @@
 <template>
     <div v-if="video">
-        {{ video.snippet.title }}
+        <div class="details">
+            <h4>{{ video.snippet.title }}</h4>
+            <p>{{ video.snippet.description }}</p>
+        </div>
     </div>
 </template>
 
@@ -15,5 +18,11 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.details {
+    margin-top: 10px;
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+}
 </style>
