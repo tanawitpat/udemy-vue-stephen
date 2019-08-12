f9270e6d3f356a44190f7368807ec329a9919759
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-08-12 15:38:36 +0700

    Section 3, Lecture 53

--- 04_Youtube/src/components/VideoList.vue
+++ 04_Youtube/src/components/VideoList.vue
@@ -1,13 +1,17 @@
 <template>
     <ul>
-        VideoList
-        {{ videos.length }}
+        <VideoListItem v-for="video in videos"/>
     </ul>
 </template>
 
 <script>
+import VideoListItem from "./VideoListItem"
+
 export default {
     name: "VideoList",
+    components: {
+        VideoListItem
+    },
     props: {
         videos: Array
     }
