cc3227d93a91cd19d2864573efa0d12d62e5a67f
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-08-12 14:51:02 +0700

    Section 2, Lecture 45

--- 04_Youtube/src/App.vue
+++ 04_Youtube/src/App.vue
@@ -5,7 +5,10 @@
 </template>
 
 <script>
+import axios from "axios"
 import SearchBar from "./components/SearchBar"
+
+const API_KEY = "AIzaSyDM_DYp9-5MUhw9CrO2DzEcrK6spblE8Vw"
 
 export default {
     name: "App",
@@ -14,7 +17,14 @@ export default {
     },
     methods: {
         onTermChange: function(searchTerm) {
-            console.log(searchTerm)
+            axios.get("https://www.googleapis.com/youtube/v3/search", {
+                params: {
+                    key: API_KEY,
+                    type: "video",
+                    part: "snippet",
+                    q: searchTerm
+                }
+            }).then(response => console.log(response))
         }
     }
 }
