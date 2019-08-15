650d6f05b71b69dc8ce209ddbe1fd45e0c664f95
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-08-15 20:33:13 +0700

    Section 6, Lecture 102

--- 07_Image-Storage/src/App.vue
+++ 07_Image-Storage/src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <AppHeader/>
+        <router-view/>
     </div>
 </template>
 
--- 07_Image-Storage/src/main.js
+++ 07_Image-Storage/src/main.js
@@ -1,8 +1,20 @@
 import Vue from "vue"
+import VueRouter from "vue-router"
 import App from "./App"
 import store from "./store"
+import AuthHandler from "./components/AuthHandler"
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode: "history",
+    routes: [
+        { path: "/oauth2/callback", component: AuthHandler }
+    ]
+})
 
 new Vue({
+    router: router,
     store,
     render: h => h(App)
 }).$mount("#app")
